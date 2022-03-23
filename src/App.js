ade2eae20b2661a1b5d54a86e51917b553c9ec8f
OMRFRKYL <omeryolu16@gmail.com>
2022-03-23 22:44:58 +0300

    değişiklik yapıldı.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.scss";
 import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header"
+import Main from "./components/Main/Main";
 
 const App = () =>{
   return(
     <div className="App">
       <Navbar/>
       <Header/>
+      <Main/>
     </div>
   )
 }
